d217501db9e6cac56423ab32e65fbf67c16628d5
GayHub1 <942418004@qq.com>
2019-12-04 15:48:26 +0800

    Signed-off-by: GayHub1 <942418004@qq.com>

--- mall_Springboot/src/main/java/com/example/mall/controller/AuthorizeController.java
+++ mall_Springboot/src/main/java/com/example/mall/controller/AuthorizeController.java
@@ -4,6 +4,8 @@ package com.example.mall.controller;
 import com.alibaba.fastjson.JSONObject;
 import com.example.mall.entity.User;
 import com.example.mall.mapper.UserMapper;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.stereotype.Controller;
@@ -12,11 +14,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import java.util.List;
+@Api(tags = "PmsBrandController", description = "商品品牌管理")
 @Controller
 public class AuthorizeController {
 
     @Autowired
     private UserMapper userMapper;
+    @ApiOperation("根据用户名获取用户列表")
     @ResponseBody
     @GetMapping("/callback")
     public String Callback(@RequestParam(name = "name") String id) {
